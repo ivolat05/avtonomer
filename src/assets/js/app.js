b11f0b9fafcf12153bdba4a0262b0f9a334053c5
Andrei Labadaev <andrei-labadaev@mail.ru>
2021-08-31 19:38:13 +0300

    update 31.08.2021 19.40 added slick

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,7 +1,16 @@
 //= components/jquery.magnific-popup.js
+//= ../../../node_modules/slick-carousel/slick/slick.js
 $(function () {
     $('.filtor__city').magnificPopup({
         type: 'inline'
+    });
+
+    $('.slider__wrapp').slick({
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
     });
 
 });
