c2908d2d8d9386f02a86a269aac53d9aa64f76e8
Andrei Labadaev <andrei-labadaev@mail.ru>
2021-09-01 17:27:51 +0300

    final 1.09.2021 17.20

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -8,9 +8,23 @@ $(function () {
     $('.slider__wrapp').slick({
         arrows: false,
         slidesToShow: 4,
+        infinite: true,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 1000,
+        responsive: [{
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2
+                }
+            }
+        ]
     });
 
 });
